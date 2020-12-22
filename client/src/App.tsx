3a0618cc15d24bbf458519edd7c96146b4bfd7de
Junior Hlongwane <juniorWebProjects@gmail.com>
2020-12-23 01:13:07 +0200

    First Commit
    TypeScript Project Setup FrontEnd

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './styles/App.css';
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>Brag Diary</h1>
+    </div>
+  );
+};

59edc4f7cb3bef9396741f24b48defe88fcc78be
Junior Hlongwane <juniorWebProjects@gmail.com>
2020-12-23 13:08:02 +0200

    Update: Home Layout (Full Responsive)
    Have the Home Layout being responsive and adding the user menu bar

--- client/src/Components/Home/Header.tsx
+++ client/src/Components/Home/Header.tsx
@@ -2,6 +2,11 @@ import React, { FC } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Logo from '../../assets/diary.svg';
 import '../../styles/Header.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import PublicIcon from '@material-ui/icons/Public';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import LockIcon from '@material-ui/icons/Lock';
 
 export const Header: FC = () => {
   return (
@@ -12,7 +17,27 @@ export const Header: FC = () => {
       <div className="nav-right">
         <h4 className="nav-link">My Diary</h4>
         <h4 className="nav-link">PD Group</h4>
-        <Avatar src="" alt="User Profile Image" />
+        <Popup
+          trigger={<Avatar src="" alt="User Profile Image" />}
+          position="bottom right"
+        >
+          <div className="user__menu">
+            <div className="menu__notAuthorised">
+              <div className="global__forum">
+                <PublicIcon />
+                <h3 className="user-menu-link">Global Forum</h3>
+              </div>
+              <div className="signin">
+                <LockOpenIcon />
+                <h3 className="user-menu-link">SignIn</h3>
+              </div>
+              <div className="signup">
+                <LockIcon />
+                <h3 className="user-menu-link">SignUp</h3>
+              </div>
+            </div>
+          </div>
+        </Popup>
       </div>
     </nav>
   );
--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -2,14 +2,12 @@ import React, { FC } from 'react';
 import { BookCover } from './BookCover';
 import { Header } from './Header';
 import '../../styles/App.css';
-import { MainContent } from './MainContent';
 
 export const Home: FC = () => {
   return (
     <div className="Home">
       <Header />
       <BookCover />
-      <MainContent />
     </div>
   );
 };

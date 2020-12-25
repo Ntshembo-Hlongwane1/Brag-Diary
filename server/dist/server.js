34c8bbdcf3cbef17c01bacbceb7e82692e7176c0
Junior Hlongwane <juniorWebProjects@gmail.com>
2020-12-26 01:50:43 +0200

    Update: Complete base server setup

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,8 +4,59 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_mongodb_session_1 = __importDefault(require("connect-mongodb-session"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = express_1.default();
-app.listen(5000, () => {
-    console.log("Server started");
+const origin = {
+    dev: "http://localhost:3000",
+    prod: "",
+};
+//=======================================================Middlewares====================================================
+app.use(cors_1.default({
+    origin: process.env.NODE_ENV === "production" ? origin.prod : origin.dev,
+    credentials: true,
+}));
+const mongoStore = connect_mongodb_session_1.default(express_session_1.default);
+const mongoURI = process.env.mongoURI;
+const store = new mongoStore({
+    collection: "usersessions",
+    uri: mongoURI,
+    expires: 365 * 60 * 60 * 24 * 1000,
+});
+const isCookieSafe = process.env.NODE_ENV === "production";
+app.use(express_session_1.default({
+    secret: process.env.session_secret,
+    name: "_sid",
+    saveUninitialized: false,
+    resave: false,
+    store: store,
+    cookie: {
+        httpOnly: true,
+        maxAge: 365 * 60 * 60 * 24 * 1000,
+        secure: isCookieSafe,
+        signed: true,
+    },
+}));
+//=================================================MongoDB Connection & Configs=========================================
+const connectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+};
+mongoose_1.default.connect(mongoURI, connectionOptions, (error) => {
+    if (error) {
+        return console.error(error);
+    }
+    return console.log("Connection MongoDB was successful");
+});
+//==============================================Server Connection & Configs=============================================
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
 });
 //# sourceMappingURL=server.js.map
--- server/src/server.ts
+++ server/src/server.ts
@@ -18,3 +18,47 @@ app.use(
     credentials: true,
   })
 );
+
+const mongoStore = MongoStore(expressSession);
+const mongoURI = process.env.mongoURI;
+const store = new mongoStore({
+  collection: "usersessions",
+  uri: mongoURI,
+  expires: 365 * 60 * 60 * 24 * 1000, //Expires after a year
+});
+const isCookieSafe = process.env.NODE_ENV === "production";
+app.use(
+  expressSession({
+    secret: process.env.session_secret,
+    name: "_sid",
+    saveUninitialized: false,
+    resave: false,
+    store: store,
+    cookie: {
+      httpOnly: true,
+      maxAge: 365 * 60 * 60 * 24 * 1000, //Expires after a year
+      secure: isCookieSafe,
+      signed: true,
+    },
+  })
+);
+
+//=================================================MongoDB Connection & Configs=========================================
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+mongoose.connect(mongoURI, connectionOptions, (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  return console.log("Connection MongoDB was successful");
+});
+
+//==============================================Server Connection & Configs=============================================
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
